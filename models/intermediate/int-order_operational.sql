930ed841a9f3fc269dca2c1b4a8f084cb2305ea7
quentin.vinger@outlook.fr <quentin.vinger@outlook.fr>
2024-12-07 15:25:36 +0000

    i

--- models/intermediate/int-order_operational.sql
+++ models/intermediate/int-order_operational.sql
@@ -1,15 +1,12 @@
-select 
-    s.orders_id, 
-    s.margin,
-    purchase_cost,
-    ROUND(s.margin + p.final_shipping_fee - p.logcost - p.ship_cost, 2) AS operational_margin
-from {{ ref('int_order_margin') }} s
+select *,
+    ROUND(margin + final_shipping_fee - logcost - ship_cost, 2) AS operational_margin
+from {{ ref('int_order_margin') }} 
 left join (
     select 
-        orders_id, 
+        orders_id,
         final_shipping_fee, 
         logcost, 
         ship_cost
     from {{ ref('stg_gz_raw__ship') }}
-) p
-on s.orders_id = p.orders_id
+) 
+using (orders_id)
